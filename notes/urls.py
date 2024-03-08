3e86ab091631fc15dc1f607d57971d0de97cc531
CoderFek <praveen.anand2212@gmail.com>
2024-03-08 23:54:04 +0530

    Second commit

--- notes/urls.py
+++ notes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import notes_add, Homeview, notes_detail
+from .views import notes_add, Homeview, notes_detail, notes_edit, notes_delete
 
 urlpatterns = [
     path('', Homeview.as_view(), name = 'home'),
     path('notes/<int:pk>/', notes_detail, name = 'notes_detail'),
     path('notes/create/', notes_add, name = 'notes_add'),
-
+    path('notes/<int:pk>/edit/', notes_edit, name='notes_edit'),
+    path('notes/<int:pk>/delete/', notes_delete, name='notes_delete'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from .models import Notes
 from django.views.generic import ListView
 from .forms import NotesForm
+import bleach
+
 # Create your views here.
 
 class Homeview(ListView):
@@ -9,12 +11,14 @@ class Homeview(ListView):
     fields = ['title']
     template_name = 'home.html'
 
+
 def notes_add(request):
     if request.method == 'POST':
         form = NotesForm(request.POST)
         if form.is_valid():
             new_instance = form.save(commit = False)
             new_instance.user = request.user
+
             new_instance.save()
             form.save()
             return redirect('home')
@@ -23,6 +27,32 @@ def notes_add(request):
         form = NotesForm()
     return render(request, 'notes_add.html', {'form':form})
 
+
 def notes_detail(request, pk):
     note = get_object_or_404(Notes, pk=pk)
-    return render(request, 'notes_detail.html', {'note':note})
+    allowed_tags = [
+        'a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'strong', 'ul',
+        'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'br', 'span'
+    ]
+    sanitized_description = bleach.clean(note.description, tags=allowed_tags, strip=True)
+    return render(request, 'notes_detail.html', {'note':note , 'sanitized_description':sanitized_description})
+
+
+def notes_edit(request, pk):
+    note = get_object_or_404(Notes, pk=pk)
+    if request.method == 'POST':
+        form = NotesForm(request.POST, instance=note)
+        if form.is_valid():
+            form.save()
+            return redirect('notes_detail', pk=pk)
+    else:
+        form = NotesForm(instance=note)
+    return render(request, 'notes_edit.html', {'form': form, 'note': note})
+
+
+def notes_delete(request, pk):
+    note = get_object_or_404(Notes, pk=pk)
+    if request.method == "POST":
+        note.delete()
+        return redirect('home')
+    return render(request, 'notes_delete.html', {'note':note})
